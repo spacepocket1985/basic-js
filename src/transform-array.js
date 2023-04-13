2d9a5a5984e6a793159f367e8e7bf4b91dd48abc
SpacePocket <space1985pocket@hmail.com>
2023-04-13 20:36:43 +0300

    refactor: update solution for transform-array.js

--- src/transform-array.js
+++ src/transform-array.js
@@ -37,7 +37,7 @@ const { NotImplementedError } = require('../extensions/index.js');
 // }
 
 function transform(arr) {
-  if (!Array.isArray(arr) || !arr instanceof Array) {
+  if (!(arr instanceof Array) || !(Array.isArray(arr))) {
     throw new Error("'arr' parameter must be an instance of the Array!");
   }
  
