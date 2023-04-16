b38422852fa7afab93f19727314fe85785656ef4
SpacePocket <space1985pocket@hmail.com>
2023-04-16 19:58:06 +0300

    feat: add solution for extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,10 +15,42 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+function repeater(str, options) {
+
+
+
+  str = String(str);
+
+
+  (typeof options.addition == "boolean" ||  options.addition === null) ? (options.addition = String(options.addition)) : ('');
+
+  (!options.addition) ? (options.addition = '') : (options.addition = String(options.addition));
+
+
+  (!options.separator) ? (options.separator = '+') : (options.separator = String(options.separator));
+
+  (!options.additionSeparator) ? (options.additionSeparator = '|') : options.additionSeparator = String(options.additionSeparator);
+
+  (!options.repeatTimes) ? (options.repeatTimes = 1) : (options.repeatTimes = Number(options.repeatTimes));
+
+  (!options.additionRepeatTimes) ? (options.additionRepeatTimes = 1) : (options.additionRepeatTimes = Number(options.additionRepeatTimes));
+
+
+  strFirstPart = (str + (options.addition + options.additionSeparator).repeat(options.additionRepeatTimes))
+  strSecondPart = strFirstPart.slice(0, strFirstPart.length - options.additionSeparator.length)
+  strThirdPart = (strSecondPart + options.separator).repeat(options.repeatTimes)
+  resultStr = strThirdPart.slice(0, strThirdPart.length - options.separator.length)
+
+
+
+  console.log('strFirstPart = ' + strFirstPart)
+  console.log('strSecondPart = ' + strSecondPart)
+  console.log('strThirdPart = ' + strThirdPart)
+  console.log('resultStr = ' + resultStr)
+  return resultStr
 }
+
 
 module.exports = {
   repeater
