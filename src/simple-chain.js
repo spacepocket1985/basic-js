3d023cb9d76b060d1fc320940cc5841f4bc1806a
SpacePocket <space1985pocket@hmail.com>
2023-04-17 22:50:07 +0300

    refactor: start solutionf for chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,25 +5,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
-  chain: [],
-
   getLength() {
-    return this.chain.length;
-
+    throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
   },
-  addLink(value) {
-
+  addLink(/* value */) {
+    throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
   },
-  removeLink(position) {
-    this.chain.reverse()
-    return this
-
+  removeLink(/* position */) {
+    throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
   },
   reverseChain() {
-
+    throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
   },
   finishChain() {
-
+    throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
   }
 };
 
